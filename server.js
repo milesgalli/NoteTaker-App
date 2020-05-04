6f94f3418f67d9244ca95acc305c1d5052d23ab4
mgalli <galli.miles@gmail.com>
2020-05-04 18:33:58 +0800

    few more changes to go

--- server.js
+++ server.js
@@ -27,11 +27,49 @@ app.get("/notes", function (req, res ){
  res.sendFile(path.join(__dirname, "notes.html"))
 })
 
+app.get("/assets/style.css", function (req, res ){
+ res.sendFile(path.join(__dirname, "assets/style.css"))
+})
 
+app.get("/assets/app.js", function (req, res ){
+ res.sendFile(path.join(__dirname, "assets/app.js"))
+})
+// clg this route to see if it is getting hit
+
+
+
+//API routes 
 
 //Show Data 
+//App.Get
+// get api/notes should read the db.json and return all saved notes as JSON 
 
 app.get("/api/notes", function (req, res ){
- res.json()
+ res.json(notes); 
+
 })
+
+// Post 
+// should recieve a a new note and save the on request body. 
+// add to db. JSON 
+//return the new note to the client 
+
+// app.post("/api/notes/", function(req, res){
+
+
+//  const newNote = req.body
+
+//  notes.push(newNote)
+
+
+
+// })
+
+// Delete 
+// 
+
+// app.post()
+
+
+
 
